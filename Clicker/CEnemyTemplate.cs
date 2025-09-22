092e6804dec0422d3de8c9d4dd7be1f340b7677c
ceokunz <faablum@mail.ru>
2025-09-22 19:40:45 +0700

    Навидываю говна на вентилятор. Класс врага и листа врагов, накашеварил нюгет жейсона ну и все в принципе. Можно офать

---
+++ Clicker/CEnemyTemplate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace Clicker
+{
+    public class CEnemyTemplate
+    {
+        [JsonInclude]
+        string name;
+        [JsonInclude]
+        string iconName;
+
+        //Атрибуты здоровья
+        [JsonInclude]
+        int baseLife;
+        [JsonInclude]
+        double lifeModifier;
+
+        //Атрибуты золота за победу над противником
+        [JsonInclude]
+        int baseGold;
+        [JsonInclude]
+        double goldModifier;
+
+        [JsonInclude]
+        double spawnChance; //Шанс на появление
+
+        public CEnemyTemplate(string Name, string IconName, int BaseLife, double LifeModifier, int BaseGold, double GoldModifier, double SpawnChance)
+        {
+            name = Name;
+            iconName = IconName;
+            baseLife = BaseLife;
+            lifeModifier = LifeModifier;
+            baseGold = BaseGold;
+            goldModifier = GoldModifier;
+            spawnChance = SpawnChance;
+        }
+    }
+}
---
+++ Clicker/CEnemyTemplateList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker
+{
+    public class CEnemyTemplateList
+    {
+
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+
+    }
+}
