9b29403df1693f297e6c8458d6ef962aa056fbb6
abc <abc>
2025-10-15 17:06:07 +0700

    2 laba

---
+++ Clicker/BigNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker
+{
+    internal class BigNumber
+    {
+        private int[] number;
+        private int Base;
+        public int ArrayLength;
+
+        public BigNumber(string number)
+        {
+
+        }
+        public BigNumber Clone()
+        {
+
+        }
+        public string ToString()
+        {
+
+        }
+        private BigNumber Add(BigNumber bnum)
+        {
+
+        }
+        private BigNumber Substruct(BigNumber bnum)
+        {
+
+        }
+        private BigNumber Multiply(double multiplier)
+        {
+
+        }
+        private BigNumber Divide (double divisor)
+        {
+
+        }
+
+
+
+
+
+    }
+}
