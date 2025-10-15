042ea51b9549019811cad4762d11ef4c5550683d
kunz <faablum@mail.ru>
2025-10-15 17:05:26 +0700

    ботаю класс плеера

---
+++ Clicker/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Clicker
+{
+    public class Player
+    {
+        int lvl;
+        BigNumber gold;
+        BigNumber damage;
+        double damageModifier;
+        BigNumber upgradeCost;
+        double upgradeModifier;
+
+        public int Lvl;
+        public BigNumber Gold;
+        public BigNumber Damage;
+        public double DamageModifier;
+        public BigNumber UpgradeCost;
+        public double UpgradeModifier;
+
+        public Player()
+        {
+
+        }
+
+        
+    }
+}
